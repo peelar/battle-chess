2b2027964e94fd5f87ab593c24598542c1a5ae74
Adrian <adrian@useo.pl>
2019-10-13 19:50:27 +0200

    Fix hook error message

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -65,7 +65,7 @@ const Grid = ({
 
     dispatchChangeTeams(initialTeams);
     dispatchChangeFields(initialFields);
-  }, []);
+  }, [dispatchChangeFields, dispatchChangeTeams]);
 
   return (
     <Container>
