a21c96793b7b4f1c66cfca60742a341d687b101f
Adrian <adrian@useo.pl>
2019-10-21 10:35:43 +0200

    Change types to enums

---
+++ src/redux/rootTypes.ts
@@ -0,0 +1,22 @@
+export enum teamsActions {
+  togglePlayerActiveness = "__TOGGLE_MEMBER_TEAMS",
+  changeTeams = "__CHANGE_TEAMS",
+  changePlayerPosition = "__CHANGE_LOCATION_TEAMS",
+  attackPlayer = "__ATTACK_PLAYER",
+  killPlayer = "__KILL_PLAYER"
+}
+
+export enum fieldsActions {
+  changeFields = "__CHANGE_FIELDS",
+  handleMove = "__HANDLE_MOVE_FIELDS",
+  setFieldsInRange = "__SET_RANGE_FIELDS",
+  clearFieldsInRange = "__CLEAR_RANGE_FIELDS"
+}
+
+export enum gameActions {
+  incrementRound = "__INCREMENT_ROUND_GAME",
+  changeActiveTeam = "__CHANGE_TEAM_GAME",
+  createEvent = "__ADD_EVENT_GAME"
+}
+
+export default {};
